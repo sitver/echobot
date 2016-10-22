8b3690cdad5d6ad3aa2fceee814999b47624cbac
Michael <editor@appstorechronicle.com>
2016-10-22 03:09:35 -0400

    First commit

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ bot.dialog('/firstRun', [
         // We'll save the users name and send them an initial greeting. All 
         // future messages from the user will be routed to the root dialog.
         session.userData.paypal = results.response;
-      users.update({ hash: session.userData.paypal, range: session.userData.name }, {       shirts: 0  })
+      users.update({ hash: 10, range: "Mikeybot" }, {       shirts: 0  })
     .then(function(resp) {
         console.log("Done logging user to DB");
     });
